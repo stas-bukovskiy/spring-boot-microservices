7eda1c3f618612319a864601558f97576c2946e0
stas-bukovskiy <stbukovskiy12@gmail.com>
2023-09-25 17:55:18 +0300

    fix: base url

--- faculty-service/src/main/java/com/recordsystem/facultyservice/config/SecurityConfiguration.java
+++ faculty-service/src/main/java/com/recordsystem/facultyservice/config/SecurityConfiguration.java
@@ -28,8 +28,8 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(request -> request.requestMatchers("/api/v1/auth/**")
-                        .permitAll().anyRequest().authenticated())
+                .authorizeHttpRequests(request -> request.requestMatchers("/api/v1/**")
+                        .hasAuthority("USER")) // TODO: change to admin
                 .sessionManagement(manager -> manager.sessionCreationPolicy(STATELESS))
                 .authenticationProvider(authenticationProvider()).addFilterBefore(
                         jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
