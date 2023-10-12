ce9a03c93181f94d1e538f0777490f3d40566b45
jester_69m <pashchuknazar@ukr.net>
2023-10-12 17:19:52 +0300

    sample of RabbitMQ

---
+++ notification-service/src/main/java/com/recordsystem/notificationservice/publisher/RabbitMqProducer.java
@@ -0,0 +1,31 @@
+package com.recordsystem.notificationservice.publisher;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RabbitMqProducer {
+
+    @Value("${rabbitmq.queue.name}")
+    private String exchange;
+
+    @Value("${rabbitmq.routing.key}")
+    private String routingKey;
+
+    private final static Logger log = LoggerFactory.getLogger(RabbitMqProducer.class);
+
+    private RabbitTemplate rabbitTemplate;
+
+    public RabbitMqProducer(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    public void sendMessage(String message) {
+        log.info(String.format("Sending message: %s", message));
+        rabbitTemplate.convertAndSend(exchange, routingKey, message);
+    }
+}
