30c183ceeb45b10433ee5a89a2388054f7a46c07
jester_69m <pashchuknazar@ukr.net>
2023-11-10 11:05:52 +0200

    add elk stack

--- user-service/src/main/java/com/recordsystem/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/recordsystem/userservice/UserServiceApplication.java
@@ -1,10 +1,7 @@
 package com.recordsystem.userservice;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.r2dbc.R2dbcAutoConfiguration;
 
 @SpringBootApplication
 public class UserServiceApplication {
