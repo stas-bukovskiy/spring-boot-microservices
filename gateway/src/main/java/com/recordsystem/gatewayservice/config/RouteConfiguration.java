b41bb621bf4b8677b64d5521c25c940273c43e47
jester_69m <pashchuknazar@ukr.net>
2023-12-07 18:18:50 +0200

    add gateway for all

--- gateway/src/main/java/com/recordsystem/gatewayservice/config/RouteConfiguration.java
+++ gateway/src/main/java/com/recordsystem/gatewayservice/config/RouteConfiguration.java
@@ -1,19 +1,11 @@
 package com.recordsystem.gatewayservice.config;
 
 import com.recordsystem.gatewayservice.filter.AuthJwtGatewayFilter;
-import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
-import io.github.resilience4j.timelimiter.TimeLimiterConfig;
 import lombok.RequiredArgsConstructor;
-import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
-import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
-import org.springframework.cloud.client.circuitbreaker.Customizer;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.function.client.WebClient;
-
-import java.time.Duration;
 
 @Configuration
 @RequiredArgsConstructor
@@ -24,16 +16,24 @@ public class RouteConfiguration {
     @Bean
     public RouteLocator myRoutes(RouteLocatorBuilder builder) {
         return builder.routes()
-                .route(p -> p
-                        .path("/get")
-                        .filters(f -> f.circuitBreaker(config -> config
-                                        .setName("myCircuitBreaker")
-                                        .setFallbackUri("forward:/fallback")))
-                        .uri("lb://localhost:8001/api/v1"))
                 .route("faculty-service", p -> p
                         .path("/faculty")
                         .filters(f -> f
                                 .rewritePath("\\/faculty", "/api/v1/faculty")
+                                .filter(authJwtGatewayFilter.apply(new AuthJwtGatewayFilter.Config())
+                        ))
+                        .uri("http://localhost:8001"))
+                .route("discipline-service", p -> p
+                        .path("/discipline")
+                        .filters(f -> f
+                                .rewritePath("\\/discipline", "/api/v1/discipline")
+                                .filter(authJwtGatewayFilter.apply(new AuthJwtGatewayFilter.Config())
+                        ))
+                        .uri("http://localhost:8001"))
+                .route("schedule-service", p -> p
+                        .path("/schedule")
+                        .filters(f -> f
+                                .rewritePath("\\/schedule", "/api/v1/schedule")
                                 .filter(authJwtGatewayFilter.apply(new AuthJwtGatewayFilter.Config())
                         ))
                         .uri("http://localhost:8001"))
@@ -47,14 +47,21 @@ public class RouteConfiguration {
                         .filters(f -> f
                                 .rewritePath("\\/users", "/api/v1/users"))
                         .uri("http://localhost:8000"))
+                .route("enrollment-service", p -> p
+                        .path("/enrollment/**")
+                        .filters(f -> f
+                                .rewritePath("\\/enrollment", "/api/v1/enrollment"))
+                        .uri("http://localhost:8003"))
+                .route("notification-service", p -> p
+                        .path("/notification/**")
+                        .filters(f -> f
+                                .rewritePath("\\/notification", "/notification"))
+                        .uri("http://localhost:8004"))
+                .route("admin-service", p -> p
+                        .path("/admin/**")
+                        .filters(f -> f
+                                .rewritePath("\\/admin", "/admin"))
+                        .uri("http://localhost:8002"))
                 .build();
-    }
-
-    @Bean
-    public Customizer<ReactiveResilience4JCircuitBreakerFactory> defaultCustomizer() {
-        return factory -> factory.configureDefault(id -> new Resilience4JConfigBuilder(id)
-                .circuitBreakerConfig(CircuitBreakerConfig.ofDefaults())
-                .timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofMillis(200)).build())
-                .build());
     }
 }
