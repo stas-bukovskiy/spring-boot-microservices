691760b0ff050c069a7c60c5c3dbd5dd8a93584f
serhii-hryhorenko <serhii.hryhorenko@teamdev.com>
2023-12-07 20:16:17 +0200

    Do JMS stuff

---
+++ notification/src/main/java/com/recordsystem/notificationservice/consumer/SystemEventConsumer.java
@@ -0,0 +1,15 @@
+package com.recordsystem.notificationservice.consumer;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class SystemEventConsumer {
+
+    @JmsListener(destination = "system_events", selector = "filterCriteria = 'important'")
+    private void handleSystemEvent(String event) {
+        log.info(event);
+    }
+}
--- notification/src/main/java/com/recordsystem/notificationservice/service/NotificationServiceImpl.java
+++ notification/src/main/java/com/recordsystem/notificationservice/service/NotificationServiceImpl.java
@@ -1,11 +1,13 @@
 package com.recordsystem.notificationservice.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class NotificationServiceImpl implements NotificationService {
     @Override
     public void sendNotification(String notificationText) {
-        // sends the notification
+        log.info(String.format("Sending notification: %s", notificationText));
     }
 }
